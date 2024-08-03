5ab0109a40edecd50cb0564045eb476e208c3840
Minh Nguyen <minhnguyen5913@gmail.com>
2024-08-02 20:00:01 -0400

    setupSQL Table Item
    https://hasura.io/learn/database/postgresql/core-concepts/6-postgresql-relationships/
    For the relationships One to One/Many

--- setup.sql
+++ setup.sql
@@ -1,6 +1,15 @@
 CREATE DATABASE pos;
 \c pos;
--- TODO: add item table here
+
+CREATE TABLE item (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	category INT NOT NULL,
+	description TEXT NOT NULL,
+	price DECIMAL(10,2)
+	CONSTRAINT fk_category FOREIGN KEY (category) REFERENCES item_category(id)
+);
+
 CREATE TABLE item_category (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(50)
