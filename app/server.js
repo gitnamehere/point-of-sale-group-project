c25f7c0244041c2cf3db5014ccbebd23f2c0b432
Andrew Nguyen <83917748+andllrew@users.noreply.github.com>
2024-08-02 20:03:53 -0400

    added create.html and updated server.js for POST API endpoint to add items in the database

--- app/server.js
+++ app/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { Pool } = require("pg");
 const env = require("../env.json");
+const { error } = require("console");
 
 const app = express();
 const hostname = "localhost";
@@ -25,6 +26,30 @@ app.get("/api/item/categories", (req, res) => {
         console.log(error);
         res.sendStatus(500);
     })
+});
+
+// POST API endpoint to add items in the database
+app.post("/api/item/add", (req, res) => {
+    const body = req.body;
+
+    if (
+        !body.hasOwnProperty("category") ||
+        !body.hasOwnProperty("description") ||
+        !body.hasOwnProperty("name") ||
+        !body.hasOwnProperty("price")
+    ) {
+        return res.sendStatus(400);
+    }
+
+    const {category, description, name, price} = body;
+
+    pool.query("INTSERT INTO item_category(category, description, name, price) VALUES($1 $2 $3 $4)", [category, description, name, price]
+        .then(result => {
+            return res.sendStatus(200);
+        })
+        .catch(error => {
+            return res.sendStatus(500);
+        }))
 });
 
 app.listen(port, hostname, () => {
