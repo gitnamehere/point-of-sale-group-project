df31b9a6c5e482fb3db380390caac31f4ed8fe55
Minh Nguyen <minhnguyen5913@gmail.com>
2024-08-02 21:48:57 -0400

    Update setup.sql
    Some format update

--- setup.sql
+++ setup.sql
@@ -1,16 +1,16 @@
 CREATE DATABASE pos;
 \c pos;
 
+CREATE TABLE item_category (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50)
+);
+
 CREATE TABLE item (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(50) NOT NULL,
 	category INT NOT NULL,
 	description TEXT NOT NULL,
-	price DECIMAL(10,2)
-	CONSTRAINT fk_category FOREIGN KEY (category) REFERENCES item_category(id)
-);
-
-CREATE TABLE item_category (
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(50)
+	price DECIMAL(10,2),
+	CONSTRAINT fk_item_category FOREIGN KEY (category) REFERENCES item_category(id)
 );
