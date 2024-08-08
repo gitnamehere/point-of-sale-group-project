879a1527d4baf72a80c19068ae7a9e019b98ebd2
Jason Huang <105529595+gitnamehere@users.noreply.github.com>
2024-08-08 17:16:01 -0400

    add layout, add example category fillers

--- app/public/pos/ordering/script.js
+++ app/public/pos/ordering/script.js
@@ -1,1 +1,11 @@
 console.log("Hello World!");
+
+let categories = document.getElementById("categories");
+
+for (let i = 0; i < 25; i++) {
+    let category = document.createElement("div");
+    category.className = "category-item"
+    category.textContent = ("Example Category " + (i + 1));
+
+    categories.appendChild(category);
+}
