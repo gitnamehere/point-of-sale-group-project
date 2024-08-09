0284212c12169c066e3661cef3c772d5b04f19a1
Jason Huang <105529595+gitnamehere@users.noreply.github.com>
2024-08-09 17:17:54 -0400

    add functionality to select categories and items from db

--- app/public/pos/ordering/script.js
+++ app/public/pos/ordering/script.js
@@ -3,16 +3,46 @@ console.log("Hello World!");
 const categoryList = document.getElementById("categories");
 const itemGrid = document.getElementById("items");
 
-let categories = ["Example 1", "Example 2", "Example 3", "Example 4"];
+function fetchItemsByCategoryId(id) {
+    fetch(`/api/items?category=${id}`)
+    .then(response => {
+        if (response.status > 200) return alert("Error while selecting category");
 
-for (let i = 0; i < categories.length; i++) {
-    let category = document.createElement("div");
-    category.className = "category-item"
-    category.textContent = categories[i];
+        return response.json()
+        .then(body => {
+            itemGrid.textContent = (body.length > 0) ? "" : "No Items Found";
 
-    category.addEventListener("click", () => {
-        alert(`Clicked on Category: ${i + 1}!`);
+            for (let i = 0; i < body.length; i++) {
+                let item = document.createElement("div");
+                item.textContent = body[i].name;
+                item.className = "item";
+                item.addEventListener("click", () => console.log(`Item Data: ${JSON.stringify(body[i])}`));
+
+                itemGrid.append(item);
+            }
+        })
+        .catch(error => console.log(error))
     })
-
-    categoryList.appendChild(category);
+    .catch(error => console.log(error))
 }
+
+fetch("/api/item/categories")
+.then(response => {
+    if (response.status > 200) return alert("Error while fetching items");
+
+    response.json().then(body => {
+        for (let i = 0; i < body.length; i++) {
+            let category = document.createElement("div");
+            category.className = "category-item";
+            category.textContent = body[i].name;
+            category.addEventListener("click", () => fetchItemsByCategoryId(body[i].id));
+
+            categoryList.appendChild(category);
+        }
+
+        // populate item grid with first category
+        fetchItemsByCategoryId(body[0].id);
+    })
+    .catch(error => console.log(error))
+})
+.catch(error => console.log(error))
