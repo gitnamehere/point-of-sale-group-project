5cebef0a2edd3efe7ca2bf57fe229fb77098d495
Minh Nguyen <minhnguyen5913@gmail.com>
2024-08-10 12:39:54 -0400

    Create authentication.js
    - Created an endpoint /api/auth/demoKey to return a token
    - Created a function to generate random token

---
+++ app/public/pos/backend-authentication/authentication.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const crypto = require("crypto");
+
+const app = express();
+const hostname = "localhost";
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(express.json());
+
+const tokenStorage = {};
+
+const authentication = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    let token = authHeader && authHeader.split(' ')[1];
+
+    if (!token || !tokenStorage[token]) {
+        return res.status(401).json({ error: "Token required."});
+    }
+    next();
+};
+
+const generateRandomToken = () => {
+    return crypto.randomBytes(32).toString("hex");
+}
+
+app.get("/api/auth/demoKey", (req, res) => {
+    const token = generateRandomToken();
+    tokenStorage[token] = true;
+    return res.json({ token });
+})
+
+app.get("/pos/", authentication, (req, res) => {
+    return res.status(200);
+});
+
+app.listen(port, hostname, () => {
+    console.log(`http://${hostname}:${port}`);
+});
