0eae94c441eada3ec2e9015248908e399690225d
mohammed <mothrowaway2003@gmail.com>
2024-08-10 21:31:29 -0400

    creating home screen for pos

---
+++ app/public/pos/homePage.js
@@ -0,0 +1,38 @@
+const newOrder = document.getElementById("newOrder");
+const activeOrders = document.getElementById("activeOrders");
+const orderHistory = document.getElementById("orderHistory");
+const itemManagement = document.getElementById("itemManagement");
+const salesReporting = document.getElementById("salesReporting");
+const storeSettings = document.getElementById("storeSettings");
+
+
+newOrder.addEventListener("click", () => {
+    newOrder.style.backgroundColor = "blue";
+    location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+})
+
+activeOrders.addEventListener("click", () => {
+    activeOrders.style.backgroundColor = "blue";
+    location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+})
+
+orderHistory.addEventListener("click", () => {
+    orderHistory.style.backgroundColor = "blue";
+    location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+})
+
+itemManagement.addEventListener("click", () => {
+    itemManagement.style.backgroundColor = "blue";
+    location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+})
+
+salesReporting.addEventListener("click", () => {
+    salesReporting.style.backgroundColor = "blue";
+    location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+})
+
+storeSettings.addEventListener("click", () => {
+    storeSettings.style.backgroundColor = "blue";
+    location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+})
+
