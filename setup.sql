486abcbf966ad05bacaed763e25469fd885c0958
Minh Nguyen <minhnguyen5913@gmail.com>
2024-08-16 17:19:54 -0400

    Update setup.sql
    - Added business information table

--- setup.sql
+++ setup.sql
@@ -44,4 +44,12 @@ CREATE TABLE account (
 	first_name TEXT NOT NULL,
 	last_name TEXT NOT NULL,
 	account_type TEXT NOT NULL
-)
+);
+
+CREATE TABLE business_information (
+	id SERIAL PRIMARY KEY,
+	business_name VARCHAR(50) NOT NULL,
+	owner_id INT NOT NULL,
+	description TEXT NOT NULL,
+	CONSTRAINT fk_owner FOREIGN KEY (owner_id) REFERENCES account(id)
+);
