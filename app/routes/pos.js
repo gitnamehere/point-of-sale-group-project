6710bb6bde8c87fced7bdcba9c3788e2336879eb
Jason Huang <105529595+gitnamehere@users.noreply.github.com>
2024-08-16 18:21:55 -0400

    use express.static in posRouter

--- app/routes/pos.js
+++ app/routes/pos.js
@@ -3,23 +3,8 @@ const posRouter = express.Router();
 const cookieParser = require("cookie-parser");
 const { authentication } = require("./api.js");
 
-// apparently setting the absolute path (probably because of node server.js) works
-// TODO: have mac user test this
-const rootPath = "./";
-
 posRouter.use(cookieParser()); // we need this here otherwise authentication wont work
 posRouter.use(authentication);
-
-posRouter.get("/ordering", (req, res) => {
-    res.sendFile("/public/pos/ordering/index.html", { root: rootPath });
-});
-
-posRouter.get("/ordering/styles.css", (req, res) => {
-    res.sendFile("/public/pos/ordering/styles.css", { root: rootPath });
-});
-
-posRouter.get("/ordering/script.js", (req, res) => {
-    res.sendFile("/public/pos/ordering/script.js", { root: rootPath });
-});
+posRouter.use(express.static("public/pos")); // we should probably move the "private" pos folder into a separate directory
 
 module.exports = { posRouter };
