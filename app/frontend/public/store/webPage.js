beba348730698fcf17dbbb164b9f0b2c35100994
Minh Nguyen <minhnguyen5913@gmail.com>
2024-08-24 20:09:39 -0400

    Delete webPage.js

--- app/frontend/public/store/webPage.js
+++
@@ -1,6 +0,0 @@
-const newOrder = document.getElementById("order");
-
-newOrder.addEventListener("click", () => {
-    newOrder.style.backgroundColor = "blue";
-    location.href = "/store/ordering";
-});
