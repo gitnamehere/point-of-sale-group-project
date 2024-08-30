7376731aff6d48671d1188e76350bce0e9529a89
Minh Nguyen <minhnguyen5913@gmail.com>
2024-08-29 20:56:10 -0400

    update order screen
    - Moved the link so that once the order being placed, it would go to payment screen
    - Added api datas from orders id and orders to the payment screen

--- app/frontend/private/pos/payment/script.js
+++ app/frontend/private/pos/payment/script.js
@@ -19,6 +19,7 @@ let discountPercentage = 0;
 let tipPercentage = 0;
 let change = 0;
 let isPaid = false; // for now until ordering screen handles payment status
+const orders = document.getElementById("orders");
 
 numPadBtns.forEach((button) => {
     button.addEventListener("click", function () {
@@ -135,6 +136,86 @@ fetch(`/api/orders/${orderId}`, {
                     .catch((error) => console.log(error));
             }
         });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+function totalQuantity(items) {
+    let total = 0;
+    for (let i in items) {
+        total += items[i];
+    }
+    return total;
+}
+
+fetch("/api/orders")
+    .then((response) => {
+        return response.json();
+    })
+    .then((body) => {
+        for (let order of body) {
+            const tr = document.createElement("tr");
+            tr.innerHTML = `
+                    <td>${order.id}</td>
+                    <td>${totalQuantity(order.items)}</td>
+                    <td>$${order.subtotal}</td>
+                `;
+
+            const detailTr = document.createElement("tr");
+            detailTr.classList.add("collapse");
+            detailTr.innerHTML = `
+                    <td colspan="6">
+                        <table class="table table-striped" id="${order.id}">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Item ID</th>
+                                    <th scope="col">Product Name</th>
+                                    <th scope="col">Quantity</th>
+                                    <th scope="col">Price</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+    
+                            </tbody>
+                        </table>
+                    </td>
+                `;
+
+            tr.addEventListener("click", () => {
+                detailTr.classList.toggle("show");
+
+                const tbody = detailTr.querySelector("tbody");
+                if (tbody.children.length === 0) {
+                    const itemIds = Object.keys(order.items);
+
+                    for (let i = 0; i < itemIds.length; i++) {
+                        let id = itemIds[i];
+
+                        fetch(`/api/items/${id}`)
+                            .then((response) => {
+                                return response.json();
+                            })
+                            .then((body) => {
+                                const itemRow = document.createElement("tr");
+                                itemRow.innerHTML = `
+                                        <td>${body[0].id}</td>
+                                        <td>${body[0].name}</td>
+                                        <td>${order.items[id]}</td>
+                                        <td>$${(body[0].price * order.items[id]).toFixed(2)}</td>
+                                    `;
+                                tbody.appendChild(itemRow);
+                            })
+                            .catch((error) => {
+                                console.log(error);
+                            });
+                    }
+                }
+            });
+
+            orders.append(tr);
+            orders.appendChild(detailTr);
+        }
     })
     .catch((error) => {
         console.log(error);
