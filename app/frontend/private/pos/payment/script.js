1ac414343534f7edbdc32d56133d613fce68da7d
Minh Nguyen <minhnguyen5913@gmail.com>
2024-08-30 20:11:30 -0400

    -Update payment screen orders items
    -Made it looks more user friendly

--- app/frontend/private/pos/payment/script.js
+++ app/frontend/private/pos/payment/script.js
@@ -153,70 +153,51 @@ function totalQuantity(items) {
 }
 
 fetch("/api/orders")
-    .then((response) => {
-        return response.json();
-    })
+    .then((response) => response.json())
     .then((body) => {
         for (let order of body) {
-            const tr = document.createElement("tr");
-            tr.innerHTML = `
-                    <td>${order.id}</td>
-                    <td>${totalQuantity(order.items)}</td>
-                    <td>$${order.subtotal}</td>
-                `;
-
             const detailTr = document.createElement("tr");
             detailTr.classList.add("collapse");
             detailTr.innerHTML = `
-                    <td colspan="6">
-                        <table class="table table-striped" id="${order.id}">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Item ID</th>
-                                    <th scope="col">Product Name</th>
-                                    <th scope="col">Quantity</th>
-                                    <th scope="col">Price</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-    
-                            </tbody>
-                        </table>
-                    </td>
-                `;
+                <td colspan="4">
+                    <table class="table table-striped" id="${order.id}">
+                        <thead>
+                            <tr>
+                                <th scope="col">Item ID</th>
+                                <th scope="col">Product Name</th>
+                                <th scope="col">Quantity</th>
+                                <th scope="col">Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                    </table>
+                </td>
+            `;
 
-            tr.addEventListener("click", () => {
-                detailTr.classList.toggle("show");
+            const tbody = detailTr.querySelector("tbody");
+            const itemIds = Object.keys(order.items);
 
-                const tbody = detailTr.querySelector("tbody");
-                if (tbody.children.length === 0) {
-                    const itemIds = Object.keys(order.items);
+            for (let i = 0; i < itemIds.length; i++) {
+                let id = itemIds[i];
 
-                    for (let i = 0; i < itemIds.length; i++) {
-                        let id = itemIds[i];
+                fetch(`/api/items/${id}`)
+                    .then((response) => response.json())
+                    .then((body) => {
+                        const itemRow = document.createElement("tr");
+                        itemRow.innerHTML = `
+                            <td>${body[0].id}</td>
+                            <td>${body[0].name}</td>
+                            <td>${order.items[id]}</td>
+                            <td>$${(body[0].price * order.items[id]).toFixed(2)}</td>
+                        `;
+                        tbody.appendChild(itemRow);
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            }
 
-                        fetch(`/api/items/${id}`)
-                            .then((response) => {
-                                return response.json();
-                            })
-                            .then((body) => {
-                                const itemRow = document.createElement("tr");
-                                itemRow.innerHTML = `
-                                        <td>${body[0].id}</td>
-                                        <td>${body[0].name}</td>
-                                        <td>${order.items[id]}</td>
-                                        <td>$${(body[0].price * order.items[id]).toFixed(2)}</td>
-                                    `;
-                                tbody.appendChild(itemRow);
-                            })
-                            .catch((error) => {
-                                console.log(error);
-                            });
-                    }
-                }
-            });
-
-            orders.append(tr);
             orders.appendChild(detailTr);
         }
     })
