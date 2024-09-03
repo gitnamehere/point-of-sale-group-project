57469e7935dcc7b7b35522af1d135f4787d8e2d3
Jason Huang <105529595+gitnamehere@users.noreply.github.com>
2024-09-02 21:27:07 -0400

    move pos route authentication to pos.js route

--- app/routes/pos.js
+++ app/routes/pos.js
@@ -1,7 +1,27 @@
 const express = require("express");
 const posRouter = express.Router();
 const cookieParser = require("cookie-parser");
-const { authentication } = require("./api.js");
+const { pool } = require("./api.js");
+
+const authentication = (req, res, next) => {
+    const { posAuth } = req.cookies;
+
+    pool.query("SELECT * FROM tokens WHERE token = $1", [posAuth])
+        .then((result) => {
+            if (result.rows.length === 0) {
+                // redirect to login page
+                // https://expressjs.com/en/4x/api.html#res.redirect
+                return res.redirect("/pos/login");
+            }
+
+            // not putting next() in here caused me an hour of pain
+            next();
+        })
+        .catch((error) => {
+            console.log(error);
+            return res.sendStatus(500);
+        });
+};
 
 posRouter.use(express.static("frontend/public/pos"));
 posRouter.use(cookieParser()); // we need this here otherwise authentication wont work
