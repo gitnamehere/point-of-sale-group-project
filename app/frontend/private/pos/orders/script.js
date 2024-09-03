71934361628d5cc73121697c56fa7c85467a8fa2
Monique Huynh <moniquehuynh280@gmail.com>
2024-09-03 17:36:33 -0400

    Ran prettier

--- app/frontend/private/pos/orders/script.js
+++ app/frontend/private/pos/orders/script.js
@@ -15,7 +15,7 @@ fetch("/api/orders")
     .then((body) => {
         for (let order of body) {
             const squareButton = document.createElement("a");
-            squareButton.href = `/pos/payment?id=${order.id}`
+            squareButton.href = `/pos/payment?id=${order.id}`;
             // there are placeholders since there isn't much information yet
             squareButton.innerHTML = `
                 <div class="order-button btn btn-light">
